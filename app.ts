77ad03726518076b61ec0668286484483c058d08
Michael <strelok.v.tanke@gmail.com>
2021-02-14 16:44:06 +0300

    one file

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
 import * as Koa from "koa";
 import * as koaBody from "koa-body";
-import { router } from "/home/mikhail/z/router/index";
+import * as Router from "koa-router";
 
 const app = new Koa();
+const router = new Router();
 
 app.use(koaBody());
 app.use(router.routes());
@@ -11,3 +12,33 @@ app.use(router.allowedMethods());
 app.listen(3000, () => {
   console.log("start server");
 });
+
+router.post("/addTask", async ctx => {
+  try {
+    const rusult = await addTask(
+      ctx.request.body.id,
+      ctx.request.body.description
+    );
+    console.log("description 1 ", ctx.request.body.id);
+    console.log("id 1 ", ctx.request.body.id);
+    ctx.body = rusult;
+  } catch (err) {
+    console.error("err", err);
+    ctx.status = 500;
+    ctx.body = " internal error";
+  }
+});
+
+router.get("/", async ctx => {
+  ctx.body = "lets go!";
+});
+
+const addTask = async (id: String, description: String) => {
+  console.log("description", description);
+  console.log("id", id);
+  try {
+    return { id, description };
+  } catch (err) {
+    console.error(err);
+  }
+};
